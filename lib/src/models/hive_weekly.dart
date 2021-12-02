7ea8370947b03bbe5adf421583be12e107786af6
tobi187 <fischerstobi1998@gmail.com>
2021-12-02 17:49:01 +0100

    weekly

--- lib/src/models/hive_weekly.dart
+++ lib/src/models/hive_weekly.dart
@@ -4,7 +4,24 @@ part 'hive_weekly.g.dart';
 
 @HiveType(typeId: 2)
 class WeeklyDBModel {
-  @HiveField(1)
+  @HiveField(0)
   String title;
-  @HiveField()
+  @HiveField(1)
+  int amount;
+  @HiveField(2)
+  DateTime creationDate;
+  @HiveField(3)
+  int priority;
+  @HiveField(4)
+  DateTime? deadline;
+  @HiveField(5)
+  String category;
+
+  WeeklyDBModel(
+      {required this.title,
+      this.amount = 0,
+      required this.creationDate,
+      this.priority = 0,
+      this.category = "",
+      this.deadline});
 }
